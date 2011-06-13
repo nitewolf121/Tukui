81687d7f4c4f852a85eb5a0e5c6f61eb0f4cf73a
Jacob Jarrett <nitewolf121@gmail.com>
2011-06-12 22:37:45 -0400

    castbar adjustment to get it back to even

--- ElvUI/modules/unitframes/uf_functions.lua
+++ ElvUI/modules/unitframes/uf_functions.lua
@@ -149,9 +149,9 @@ E.LoadUFFunctions = function(layout)
 			button:SetTemplate("Default")
 			button:SetBackdropBorderColor(unpack(C["media"].bordercolor))
 			if direction == "LEFT" then
-				button:Point("RIGHT", castbar, "LEFT", -4*E.ResScale, 0)
+				button:Point("RIGHT", castbar, "LEFT", -3*E.ResScale, 0)
 			else
-				button:Point("LEFT", castbar, "RIGHT", 4*E.ResScale, 0)
+				button:Point("LEFT", castbar, "RIGHT", 3*E.ResScale, 0)
 			end
 			
 			castbar.Icon = button:CreateTexture(nil, "ARTWORK")
